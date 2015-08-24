f910abd0ebb10f006ae4a80b942186e001362b5f
muskan21 <muskan.cs.du/2013@gmail.com>
2015-08-24 17:45:30 +0530

    Item Class

---
+++ src/Item.java
@@ -0,0 +1,10 @@
+/**
+ * Created by muskandhanda on 8/24/15.
+ */
+public class Item {
+    private String itemName;
+    private boolean itemTypeInExempt;
+    private double salesTax;
+    private double itemPrice;
+    private int quantity;
+}
