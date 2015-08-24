da923a7d82491c3435d959bc84cd9e17ed76b1c1
muskan21 <muskan.cs.du/2013@gmail.com>
2015-08-24 17:53:45 +0530

    Menu Display

--- src/BaselineApp.java
+++ src/BaselineApp.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 /**
  * Created by muskandhanda on 8/24/15.
  */
 public class BaselineApp {
-    System.out.println("Welcome to Baseline!");
+    public static void main(String []args) {
+        System.out.println("Welcome to Baseline!\nStart Shopping!");
+        Scanner input = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("\nMenu.\n1.Add new item.\n2.Print Result.\n3.Quit.\nEnter your choice : ");
+            choice = input.nextInt();
+        }while(choice != 3)
+    }
 }
